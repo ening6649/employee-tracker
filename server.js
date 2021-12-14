b72b0de85ea0cce96774804ff03f29abb142c9bb
ening <ening6649@yahoo.com>
2021-12-14 15:07:34 -0500

    initial setup

---
+++ server.js
@@ -0,0 +1,9 @@
+const db = require ('./db/connection')
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  });
